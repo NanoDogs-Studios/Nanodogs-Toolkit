6a358f6a1a1a1414068de95f38f407c1b296b0ff
Dylan Day <Dylan101day@gmail.com>
2025-10-05 02:45:55 +0100

    bake lightmap

--- Editor/NanodogsToolkit/NanodogsImporter.cs
+++ Editor/NanodogsToolkit/NanodogsImporter.cs
@@ -9,35 +9,37 @@ namespace Nanodogs.Toolkit
 {
     public class NanodogsToolkitImporter
     {
+        public static string baseURL = "Assets/Plugins/Nanodogs-Toolkit/Editor/NanodogsToolkit/Import/";
+
         [MenuItem("Nanodogs/Importer/Import Modular FPS Package")]
         private static void importModularFPS()
         {
-            string package = "Assets/Editor/Import/Modular First Person Controller.unitypackage"; // I wanted to leave out specifics, so this is just a sample link.
+            string package = baseURL + "Modular First Person Controller.unitypackage"; // I wanted to leave out specifics, so this is just a sample link.
             AssetDatabase.ImportPackage(package, false);
         }
         [MenuItem("Nanodogs/Importer/Import Mirror")]
         private static void importMirror()
         {
-            string package = "Assets/Editor/Import/Mirror.unitypackage"; // I wanted to leave out specifics, so this is just a sample link.
+            string package = baseURL + "Mirror.unitypackage"; // I wanted to leave out specifics, so this is just a sample link.
             AssetDatabase.ImportPackage(package, false);
         }
         [MenuItem("Nanodogs/Importer/Import LowPoly Water")]
         private static void importLowPolyWater()
         {
-            string package = "Assets/Editor/Import/LowPoly Water.unitypackage"; // I wanted to leave out specifics, so this is just a sample link.
+            string package = baseURL + "Assets/Editor/Import/LowPoly Water.unitypackage"; // I wanted to leave out specifics, so this is just a sample link.
             AssetDatabase.ImportPackage(package, false);
         }
         [MenuItem("Nanodogs/Importer/Import Mesh Combiner")]
         private static void importMeshCombiner()
         {
-            string package = "Assets/Editor/Import/Mesh Combiner.unitypackage"; // I wanted to leave out specifics, so this is just a sample link.
+            string package = baseURL + "Assets/Editor/Import/Mesh Combiner.unitypackage"; // I wanted to leave out specifics, so this is just a sample link.
             AssetDatabase.ImportPackage(package, false);
         }
 
         [MenuItem("Nanodogs/Importer/Import Volumetric Light Beam")]
         private static void importNewInput()
         {
-            string package = "Assets/Editor/Import/VolumetricLightBeam.unitypackage"; // I wanted to leave out specifics, so this is just a sample link.
+            string package = baseURL + "Assets/Editor/Import/VolumetricLightBeam.unitypackage"; // I wanted to leave out specifics, so this is just a sample link.
             AssetDatabase.ImportPackage(package, false);
         }
     }

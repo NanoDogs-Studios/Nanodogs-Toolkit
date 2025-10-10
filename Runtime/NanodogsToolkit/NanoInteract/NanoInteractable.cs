d7413e533c5b5ed18755f844153f88721dbfba44
Dylan Day <Dylan101day@gmail.com>
2025-10-10 13:08:51 +0100

    add nanointeract and nanopooler

---
+++ Runtime/NanodogsToolkit/NanoInteract/NanoInteractable.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Nanodogs.UniversalScripts
+{
+    [RequireComponent(typeof(Collider))] // Ensures it has a collider
+    public class NanoInteractable : MonoBehaviour
+    {
+        [Header("Interaction Settings")]
+        [Tooltip("Can the player currently interact with this object?")]
+        public bool isCurrentlyInteractable = true;
+
+        [Header("Events")]
+        public UnityEvent onInteract;
+
+        /// <summary>
+        /// Called when the player interacts with this object.
+        /// </summary>
+        public virtual void Interact()
+        {
+            if (!isCurrentlyInteractable) return;
+
+            Debug.Log($"{gameObject.name} got interacted with!");
+            onInteract?.Invoke();
+            SetInteractable(false);
+        }
+
+        public void SetInteractable(bool value)
+        {
+            isCurrentlyInteractable = value;
+        }
+    }
+}
---
+++ Runtime/NanodogsToolkit/Templates/NanoPlayerInteractor.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace Nanodogs.UniversalScripts
+{
+    public class NanoPlayerInteractor : MonoBehaviour
+    {
+        [Header("Interaction Settings")]
+        [Tooltip("Key used to interact with objects.")]
+        public KeyCode interactKey = KeyCode.E;
+
+        [Tooltip("Maximum distance to interactable objects.")]
+        public float interactDistance = 3f;
+
+        [Tooltip("Layer mask for interactable objects.")]
+        public LayerMask interactableLayer;
+
+        [Tooltip("Camera used for raycasting.")]
+        public Camera cam;
+
+        private void Update()
+        {
+            CheckForInteraction();
+        }
+
+        private void CheckForInteraction()
+        {
+            Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)); // center of screen
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, interactDistance, interactableLayer))
+            {
+                NanoInteractable interactable = hit.collider.GetComponent<NanoInteractable>();
+                if (interactable != null && interactable.isCurrentlyInteractable)
+                {
+                    // Optional: highlight object here
+
+                    if (Input.GetKeyDown(interactKey))
+                    {
+                        interactable.Interact();
+                    }
+                }
+            }
+        }
+    }
+}

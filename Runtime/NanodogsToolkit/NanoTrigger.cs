fa329f5197992b87f1f84930ecef67fd280a6426
Dylan Day <Dylan101day@gmail.com>
2025-10-11 19:32:09 +0100

    Update NanoTrigger.cs

--- Runtime/NanodogsToolkit/NanoTrigger.cs
+++ Runtime/NanodogsToolkit/NanoTrigger.cs
@@ -1,11 +1,27 @@
 ﻿// © 2025 Nanodogs Studios. All rights reserved.
 
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Nanodogs.UniversalScripts
 {
     public class NanoTrigger : MonoBehaviour
     {
+        public UnityAction onTriggerEnter;
+        public UnityAction onTriggerExit;
+        public UnityAction onTriggerStay;   
 
+        private void OnTriggerEnter(Collider other)
+        {
+            onTriggerEnter?.Invoke();
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            onTriggerExit?.Invoke();
+        }
+        private void OnTriggerStay(Collider other)
+        {
+            onTriggerStay?.Invoke();
+        }
     }
 }
